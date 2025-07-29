dd2884c10944029558fbd186a2bd00f20b2b0da5
kazuma0127 <azd0127@gmail.com>
2025-07-29 22:05:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-from flask import Flask, request, jsonify, send_file
-import pandas as pd
-import os
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Excel API is running!'
-
 @app.route('/create-excel', methods=['POST'])
 def create_excel():
     data = request.get_json()
@@ -22,10 +12,10 @@ def create_excel():
         filename = f"{title}.xlsx"
         df.to_excel(filename, index=False)
 
-        return send_file(filename, as_attachment=True)
+        return send_file(
+            filename,
+            as_attachment=True,
+            mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
